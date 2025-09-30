694f973ad361cc83e4fb355c7ccc3b2495e40647
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2025-09-30 11:36:41 +0200

    Add option to set custom configuration prefix
    Signed-off-by: Jean-Yves <7360784+docjyJ@users.noreply.github.com>

--- apps/user_ldap/lib/Command/CreateEmptyConfig.php
+++ apps/user_ldap/lib/Command/CreateEmptyConfig.php
@@ -5,6 +5,7 @@
  * SPDX-FileCopyrightText: 2016 ownCloud, Inc.
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+
 namespace OCA\User_LDAP\Command;
 
 use OCA\User_LDAP\Configuration;
@@ -12,6 +13,7 @@ use OCA\User_LDAP\Helper;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CreateEmptyConfig extends Command {
@@ -31,11 +33,33 @@ class CreateEmptyConfig extends Command {
 				InputOption::VALUE_NONE,
 				'outputs only the prefix'
 			)
-		;
+			->addOption(
+				'set-prefix',
+				's',
+				InputOption::VALUE_OPTIONAL,
+				'manually set the prefix (must be unique)',
+			);
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
-		$configPrefix = $this->helper->getNextServerConfigurationPrefix();
+		$configPrefix = $input->getOption('set-prefix');
+		if (is_string($configPrefix)) {
+			if ($configPrefix === '') {
+				$output->writeln('The prefix cannot be empty');
+				return self::FAILURE;
+			}
+			if (preg_match('/^[a-zA-Z0-9-_]+$/', $configPrefix) !== 1) {
+				$output->writeln('The prefix may only contain alphanumeric characters, dashes and underscores');
+				return self::FAILURE;
+			}
+			$availableConfigs = $this->helper->getServerConfigurationPrefixes();
+			if (in_array($configPrefix, $availableConfigs)) {
+				$output->writeln('The prefix is already in use');
+				return self::FAILURE;
+			}
+		} else {
+			$configPrefix = $this->helper->getNextServerConfigurationPrefix();
+		}
 		$configHolder = new Configuration($configPrefix);
 		$configHolder->ldapConfigurationActive = false;
 		$configHolder->saveConfiguration();
